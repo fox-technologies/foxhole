212cfcd9816470172ef32244e43b9761ea2876ed
Callum Osborn <callum.osborn@smartaction.com>
2019-06-27 15:35:17 -0700

    Format Django project files
    - Change indentation size to 2 spaces
    - Remove unnecessary comments

--- foxhole/manage.py
+++ foxhole/manage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foxhole.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foxhole.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,6 +14,5 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
